dc6cda4fcf72e6d6d70cc200eae8c93325bfe39c
Tutti Quintella <q.tuane@gmail.com>
2020-02-20 23:25:52 +0900

    Make post card simpler

--- components/cards/PostCard.vue
+++ components/cards/PostCard.vue
@@ -13,10 +13,10 @@
       v-if="author && $siteConfig.posts.displayAuthor"
       class="author-wrapper"
     >
-      <strong>Author:</strong> {{ author }} |
+      <strong>{{ author }}</strong> |
     </span>
     <span v-if="date" class="date-wrapper">
-      <strong>Published on:</strong> {{ datePretty }}
+      {{ datePretty }}
     </span>
   </generic-card>
 </template>
