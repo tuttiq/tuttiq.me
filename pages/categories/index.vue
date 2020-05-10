f070eaddd034c911de341c20ebe265ecac928633
Tutti Quintella <q.tuane@gmail.com>
2020-05-10 23:37:01 +0900

    add multi-select tag list to content page

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -33,6 +33,27 @@
             {{ cat.name }}
           </nuxt-link>
         </div>
+        <h3 class="subtitle with-title-link">
+          Tags
+        </h3>
+        <a
+          class="title-link has-text-grey-light has-text-weight-bold is-size-7"
+          @click="clearTags()"
+        >
+          <font-awesome-icon icon="times" class="clear-icon" />
+          Clear
+        </a>
+        <div class="panel tags">
+          <a
+            v-for="tag in allTags"
+            :key="tag.slug"
+            :class="['tag', isSelected(tag) ? 'is-dark' : 'is-primary']"
+            @click="toggleTag(tag)"
+          >
+            {{ tag.name }}
+            <button v-if="isSelected(tag)" class="delete is-small"></button>
+          </a>
+        </div>
       </template>
     </main-section>
   </div>
@@ -46,11 +67,51 @@ export default {
   },
   data() {
     return {
-      allCats: []
+      allCats: [],
+      allTags: [],
+      selectedTags: []
     }
   },
   async created() {
     this.allCats = await this.$cms.category.getAll()
+    this.allTags = await this.$cms.tag.getAll()
+  },
+  methods: {
+    clearTags() {
+      this.selectedTags = []
+    },
+    toggleTag(tag) {
+      if (this.isSelected(tag)) {
+        const index = this.selectedTags.indexOf(tag.slug)
+        this.selectedTags.splice(index, 1)
+      } else {
+        this.selectedTags.push(tag.slug)
+      }
+    },
+    isSelected(tag) {
+      return this.selectedTags.indexOf(tag.slug) !== -1
+    }
   }
 }
 </script>
+
+<style>
+.with-title-link {
+  display: inline-block;
+}
+.title-link {
+  display: inline-block;
+  margin-left: 1em;
+  vertical-align: middle;
+}
+.clear-icon {
+  margin-right: 0.25em;
+}
+a.tag {
+  opacity: 0.93;
+}
+a.tag:hover {
+  text-decoration: none;
+  opacity: 1;
+}
+</style>
