1f0e6a96fff02e68f3e0786a9f88dfb29d5d930d
Tutti Quintella <q.tuane@gmail.com>
2020-09-27 16:09:25 +0900

    update projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -126,6 +126,18 @@
                 in these areas and have some networking time over food and
                 drinks.
               </p>
+              <p>
+                Although we haven't been able to organize any in-person events
+                since COVID-19, The Lead Dev has been holding some online events
+                and they even started an online publication, with some great
+                articles written by industry leaders. I also recently published
+                my article with them:
+                <a
+                  href="https://leaddev.com/data-centric-approach-understanding-underrepresentation-and-its-impact"
+                >
+                  A data-centric approach to understanding underrepresentation.
+                </a>
+              </p>
             </div>
             <div class="column">
               <figure class="image">
@@ -180,6 +192,70 @@
               to become a host or sponsor
             </li>
           </ul>
+        </div>
+
+        <div id="speakherjp" class="content project">
+          <h3>SpeakHer.jp</h3>
+          <p>
+            <a href="https://speakher.jp">
+              https://speakher.jp
+            </a>
+            <br />
+            <a href="https://github.com/WWCodeTokyo/speak-her-db">
+              SpeakHer on Github
+            </a>
+          </p>
+          <div class="columns is-desktop">
+            <div class="column">
+              <p>
+                This idea came from my friend
+                <a href="https://twitter.com/yanarchy">
+                  Yan Fan
+                </a>
+                her friend
+                <a href="https://linkedin.com/in/emitakemura/">
+                  Emi Takemura
+                </a>
+                as a way to uncover and give vibility to women public speakers
+                in Japan, encouraging more diversity in conference lineups and
+                panels. So I gathered Yan and
+                <a href="https://twitter.com/stonecoldkilzer">
+                  Ann Kilzer
+                </a>
+                and we started working on this project together.
+              </p>
+              <p>
+                The project is (for now) a serverless
+                <a href="https://vuejs.org/">
+                  Vue.js
+                </a>
+                site, using
+                <a href="https://www.airtable.com/">
+                  Airtable
+                </a>
+                as the database and hosted on
+                <a href="https://www.netlify.com/">
+                  Netlify
+                </a>
+                . It's also
+                <a href="https://github.com/WWCodeTokyo/speak-her-db">
+                  an open-source project
+                </a>
+                being developed under the the Women Who Code Tokyo github
+                organization.
+              </p>
+            </div>
+            <div class="column">
+              <figure class="image">
+                <opti-image
+                  :src="require('~/assets/uploads/speakher_banner.png').src"
+                  :srcset="
+                    require('~/assets/uploads/speakher_banner.png').srcSet
+                  "
+                />
+              </figure>
+            </div>
+          </div>
         </div>
 
         <div id="tuttiqme" class="content project">
@@ -270,28 +346,6 @@
               </ul>
             </li>
             <li>
-              Something related to Data Science and Machine Learning (I know,
-              very specific!)
-              <ul>
-                <li>
-                  I took a high level
-                  <a href="https://www.coursera.org/learn/ai-for-everyone">
-                    AI Course
-                  </a>
-                  on <a href="https://www.coursera.org/">Coursera</a> and got
-                  super excited about it. Then I started another course on
-                  <a
-                    href="https://www.coursera.org/specializations/data-science-python"
-                  >
-                    Applied Data Science with Python
-                  </a>
-                  (which, honestly, have not been nearly as exciting lol). I'm
-                  still on brainstroming phase, but eventually I want to try out
-                  a first project using Data Science and ML.
-                </li>
-              </ul>
-            </li>
-            <li>
               Structured courses and workshops
               <ul>
                 <li>
@@ -320,15 +374,102 @@
       </template>
 
       <template v-slot:sidebar>
-        <a
-          class="twitter-timeline"
-          data-lang="en"
-          data-theme="light"
-          data-height="600"
-          href="https://twitter.com/WWCode_Tokyo?ref_src=twsrc%5Etfw"
-        >
-          Tweets by WWCode_Tokyo
-        </a>
+        <div class="content has-text-centered">
+          <h2>On Twitter</h2>
+          <blockquote class="twitter-tweet">
+            <p lang="en" dir="ltr">
+              Are you still social distancing but miss talking to other people?
+              Join our online networking party and get to know other women
+              passionate in technology!<br /><br />
+              Saturday, Sep 26th at 15:00, we&#39;ll use Spatial Chat to chat in
+              groups and have some music!
+              <a
+                href="https://twitter.com/hashtag/WWCode?src=hash&amp;ref_src=twsrc%5Etfw"
+              >
+                #WWCode
+              </a>
+              <a href="https://t.co/gjutuaeIGp">https://t.co/gjutuaeIGp</a>
+            </p>
+            &mdash; Women Who Code Tokyo (@WWCode_Tokyo)
+            <a
+              href="https://twitter.com/WWCode_Tokyo/status/1305818882389602307?ref_src=twsrc%5Etfw"
+            >
+              September 15, 2020
+            </a>
+          </blockquote>
+
+          <blockquote class="twitter-tweet">
+            <p lang="en" dir="ltr">
+              What does underrepresentation, and the gender gap in particular,
+              really look like in today&#39;s tech industry?
+              <a href="https://twitter.com/tuttiq?ref_src=twsrc%5Etfw">
+                @tuttiq
+              </a>
+              shares her observations in this article.
+              <a href="https://t.co/0IdUAwgOSu">https://t.co/0IdUAwgOSu</a>
+            </p>
+            &mdash; LeadDev (@TheLeadDev)
+            <a
+              href="https://twitter.com/TheLeadDev/status/1303670238744055808?ref_src=twsrc%5Etfw"
+            >
+              September 9, 2020
+            </a>
+          </blockquote>
+
+          <blockquote class="twitter-tweet">
+            <p lang="en" dir="ltr">
+              Hello! We created a website to help empower
+              <a
+                href="https://twitter.com/hashtag/women?src=hash&amp;ref_src=twsrc%5Etfw"
+              >
+                #women
+              </a>
+              speakers in Japan. Please share and nominate women who are
+              interested in public speaking.
+              <a
+                href="https://twitter.com/hashtag/publicSpeaking?src=hash&amp;ref_src=twsrc%5Etfw"
+              >
+                #publicSpeaking
+              </a>
+              <a
+                href="https://twitter.com/hashtag/Japan?src=hash&amp;ref_src=twsrc%5Etfw"
+              >
+                #Japan
+              </a>
+              <a
+                href="https://twitter.com/hashtag/womenLeaders?src=hash&amp;ref_src=twsrc%5Etfw"
+              >
+                #womenLeaders
+              </a>
+              <a href="https://t.co/shgcaFUQ56">https://t.co/shgcaFUQ56</a>
+            </p>
+            &mdash; SpeakHer Japan (@speakherjapan)
+            <a
+              href="https://twitter.com/speakherjapan/status/1300416493260161025?ref_src=twsrc%5Etfw"
+            >
+              August 31, 2020
+            </a>
+          </blockquote>
+
+          <blockquote class="twitter-tweet">
+            <p lang="en" dir="ltr">
+              I was so focused (or daydreaming?) while working on
+              <a href="https://twitter.com/speakherjapan?ref_src=twsrc%5Etfw">
+                @speakherjapan
+              </a>
+              today that my bf came into the room, placed a bottle on my head
+              and I still didn&#39;t move for long enough that he could take
+              this stupid photo.
+              <a href="https://t.co/3NKPBK9kzf">pic.twitter.com/3NKPBK9kzf</a>
+            </p>
+            &mdash; Tutti Q. 🍒 (@tuttiq)
+            <a
+              href="https://twitter.com/tuttiq/status/1307287003143127069?ref_src=twsrc%5Etfw"
+            >
+              September 19, 2020
+            </a>
+          </blockquote>
+        </div>
       </template>
     </main-section>
   </div>
@@ -349,7 +490,10 @@ export default {
 }
 </script>
 <style>
-.project {
+.project:not(:first-child) {
   padding-top: 1em;
+}
+.is-one-quarter.right-sidebar {
+  border: none;
 }
 </style>
